c440697354dacf96923932273a0620933fe54799
Luan <luan.decastro@townsq.com.br>
2023-04-30 00:39:56 -0300

    Project creation

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HomeComponent} from './views/home/home.component'
+import {UserCrudComponent} from './views/user-crud/user-crud.component'
+
+
+const routes: Routes = [
+    {
+      path: "",
+      component: HomeComponent
+    },
+    {
+      path: "users",
+      component: UserCrudComponent
+    },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/views/user-crud/user-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-crud',
+  templateUrl: './user-crud.component.html',
+  styleUrls: ['./user-crud.component.css']
+})
+export class UserCrudComponent {
+
+}
